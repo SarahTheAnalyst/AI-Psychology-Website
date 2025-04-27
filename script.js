c0b48bc798e18a22e7331fd97142ef76e9310d88
SarahTheAnalyst <sarahtheanalyst@outlook.com>
2025-04-27 02:20:03 -0400

    Quick update

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 window.addEventListener('load', () => {
     document.body.classList.add('loaded');
   });
+
+  //FIX THE BUBBLES FOR THE PHONES
+  document.querySelectorAll('.bubble').forEach(bubble => {
+    bubble.addEventListener('click', () => {
+      if (!bubble.classList.contains('revealed')) {
+        bubble.classList.add('revealed');
+        bubble.innerHTML = bubble.getAttribute('data-info');
+      } else {
+        bubble.classList.remove('revealed');
+        bubble.innerHTML = bubble.getAttribute('data-term');
+      }
+    });
+  });
   
